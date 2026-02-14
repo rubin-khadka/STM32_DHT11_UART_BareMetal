031ab5120d95638660efb024b41c7ba43a47a062
rubin-khadka <rubin.robotic@gmail.com>
2026-02-14 22:13:35 +0100

    update uart files, with setting circular buffer

--- Inc/uart.h
+++ Inc/uart.h
@@ -8,6 +8,35 @@
 #ifndef UART_H_
 #define UART_H_
 
+#include "stdint.h"
+#include "stdbool.h"
+
+// Circular Buffer Structure for USART1
+typedef struct
+{
+  uint8_t          *buffer; 	// Pointer to buffer memory
+  uint16_t          size;   	// Total buffer size
+  volatile uint16_t head;   	// Write Position
+  volatile uint16_t tail;   	// Read Position
+  volatile uint16_t count;  	// Number of bytes in the buffer
+  volatile bool     overflow;   // Overflow flag
+} UART_Buffer_t;
+
+// External declarations
+extern volatile UART_Buffer_t usart1_rx_buf;
+extern volatile UART_Buffer_t usart1_tx_buf;
+
+// Function Definitions
 void USART1_Init(void);
+
+// Core buffer functions
+void UART1_Buffer_Init(volatile UART_Buffer_t *buff, uint8_t *storage, uint16_t size);
+bool UART1_Buffer_Full(volatile UART_Buffer_t *buff);
+bool UART1_Buffer_Empty(volatile UART_Buffer_t *buff);
+bool UART1_Buffer_Write(volatile UART_Buffer_t *buff, uint8_t data);
+uint8_t UART1_Buffer_Read(volatile UART_Buffer_t *buff);
+
+// Interrupt function
+void USART1_IRQHandler(void);
 
 #endif /* UART_H_ */
--- Src/uart.c
+++ Src/uart.c
@@ -6,6 +6,17 @@
  */
 
 #include "stm32f103xb.h"
+#include "uart.h"
+
+// Define buffer size
+#define USART1_RX_BUF_SIZE 256  // RX buffer size
+#define USART1_TX_BUF_SIZE 256  // TX buffer size
+
+// Global buffer instances for USART1
+uint8_t usart1_rxbuf_storage[USART1_RX_BUF_SIZE];
+uint8_t usart1_txbuf_storage[USART1_TX_BUF_SIZE];
+volatile UART_Buffer_t usart1_rx_buf;
+volatile UART_Buffer_t usart1_tx_buf;
 
 void USART1_Init(void)
 {
@@ -22,7 +33,7 @@ void USART1_Init(void)
 	GPIOA->CRH |= GPIO_CRH_CNF10_0;	// CNF = 01 (Floating input)
 
 	// Set Baud Rate
-	USART1->BRR = 0x271;
+	USART1->BRR = 0x271; // 115200 baud rate
 
 	// Disable USART1 temporarily
 	USART1->CR1 &= ~USART_CR1_UE;
@@ -40,5 +51,85 @@ void USART1_Init(void)
 	USART1->CR1 |= USART_CR1_UE;
 }
 
+void UART1_Buffer_Init(volatile UART_Buffer_t *buff, uint8_t *storage, uint16_t size)
+{
+  buff->buffer = storage;
+  buff->size = size;
+  buff->head = 0;
+  buff->tail = 0;
+  buff->count = 0;
+  buff->overflow = false;
+}
+
+bool UART1_Buffer_Full(volatile UART_Buffer_t *buff)
+{
+  return (buff->count >= buff->size);
+}
+
+bool UART1_Buffer_Empty(volatile UART_Buffer_t *buff)
+{
+  return (buff->count == 0);
+}
+
+bool UART1_Buffer_Write(volatile UART_Buffer_t *buff, uint8_t data)
+{
+  uint32_t primask = __get_PRIMASK();
+  __disable_irq();
+
+  if(UART1_Buffer_Full(buff))
+  {
+    buff->overflow = true;
+    __set_PRIMASK(primask);
+    return false;
+  }
+
+  buff->buffer[buff->head] = data;
+  buff->head = (buff->head + 1) % buff->size;
+  buff->count++;
+
+  __set_PRIMASK(primask);
+
+  return true;
+}
+
+uint8_t UART1_Buffer_Read(volatile UART_Buffer_t *buff)
+{
+  uint32_t primask = __get_PRIMASK();
+  __disable_irq();
+
+  if(UART1_Buffer_Empty(buff))
+  {
+    __set_PRIMASK(primask);
+    return 0;
+  }
+
+  uint8_t data = buff->buffer[buff->tail];
+  buff->tail = (buff->tail + 1) % buff->size;
+  buff->count--;
+
+  __set_PRIMASK(primask);
+  return data;
+}
+
+// Interrupt Functions
+void USART1_IRQHandler(void)
+{
+  // Check if RXNE interrupt
+  if(USART1->SR & USART_SR_RXNE)
+  {
+    uint8_t data = USART1->DR;
+    UART1_Buffer_Write(&usart1_rx_buf, data);
+  }
+
+  // TX interrupt - reads from TX buffer
+  if((USART1->CR1 & USART_CR1_TXEIE) && (USART1->SR & USART_SR_TXE))
+  {
+    if(!UART1_Buffer_Empty(&usart1_tx_buf))
+    {
+      uint8_t data = UART1_Buffer_Read(&usart1_tx_buf);  // READS from TX buffer
+      USART1->DR = data;
+    }
+  }
+}
 
 
