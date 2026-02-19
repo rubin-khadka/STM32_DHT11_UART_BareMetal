2a09d2e3d1dfaeee3e1e01f6482b6f2fd339bf7d
rubin-khadka <rubin.robotic@gmail.com>
2026-02-19 15:01:53 +0100

    update clock source to 8 MHz

--- Src/system_stm32f1xx.c
+++ Src/system_stm32f1xx.c
@@ -9,33 +9,28 @@
 #include "stdint.h"
 
 // System Core Clock Frequency
-uint32_t SystemCoreClock = 72000000;
+uint32_t SystemCoreClock = 8000000; 
 
-void SystemInit(void) {
-	// Enable HSE (8MHz external crystal)
-	RCC->CR |= RCC_CR_HSEON;
-	while ((RCC->CR & RCC_CR_HSERDY) == 0);
-
-	// Configure Flash for 72MHz (2 wait states)
-	FLASH->ACR = FLASH_ACR_LATENCY_2 | FLASH_ACR_PRFTBE;
-
-	// Configure PLL: 8MHz × 9 = 72MHz
-	RCC->CFGR &= ~(RCC_CFGR_PLLMULL | RCC_CFGR_PLLSRC | RCC_CFGR_PLLXTPRE);
-	RCC->CFGR |= RCC_CFGR_PLLMULL9;       	// Multiply by 9
-	RCC->CFGR |= RCC_CFGR_PLLSRC;     		// Use HSE as source
-
-	// APB1 prescaler: divide by 2 (max 36MHz)
-	RCC->CFGR &= ~RCC_CFGR_PPRE1;
-	RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
-
-	// Enable PLL and wait for lock
-	RCC->CR |= RCC_CR_PLLON;
-	while ((RCC->CR & RCC_CR_PLLRDY) == 0);
-
-	// Switch system clock to PLL
-	RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_SW) | RCC_CFGR_SW_PLL;
-	while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
-
+void SystemInit(void)
+{
+	// Enable HSI (8MHz internal RC oscillator)
+	RCC->CR |= RCC_CR_HSION;
+	while((RCC->CR & RCC_CR_HSIRDY) == 0);
+	
+	// Configure Flash for 8MHz 
+	FLASH->ACR = FLASH_ACR_PRFTBE;
+	
+	// Clear PLL and clock configuration 
+	RCC->CFGR &= ~(RCC_CFGR_SW | RCC_CFGR_PLLMULL | RCC_CFGR_PLLSRC);
+	
+	// Switch system clock to HSI
+	RCC->CFGR |= RCC_CFGR_SW_HSI;
+	while((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI);
+	
+	// Turn off HSE and PLL to save power
+	RCC->CR &= ~RCC_CR_HSEON;
+	RCC->CR &= ~RCC_CR_PLLON;
+	
 	// Update SystemCoreClock
-	SystemCoreClock = 72000000;
+	SystemCoreClock = 8000000;
 }
